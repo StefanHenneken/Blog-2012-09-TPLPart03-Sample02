55d0884e687552cb22e247f9cddb2d928ad05243
Stefan Henneken <stefan.henneken@live.com>
2017-06-04 00:00:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sample02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            int x = 0;
+            // int x = 1;
+
+            Task<int> taskRoot = new Task<int>((para) =>
+            {
+                Console.WriteLine("taskRoot x ist: {0}", (int)para);
+                int y = 1 / (int)para;
+                return y;
+            }, x);
+
+            taskRoot.ContinueWith((taskSuccess) =>
+            {
+                Console.WriteLine("taskSuccess");
+            }, TaskContinuationOptions.NotOnFaulted);
+
+            taskRoot.ContinueWith((taskError) =>
+            {
+                Console.WriteLine("taskError");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            taskRoot.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
